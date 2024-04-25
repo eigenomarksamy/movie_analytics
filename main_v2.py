f0f2d7f72051703bf847c3b75de3bf068ca010cc
eigenomar <omarkhaledsamy20@gmail.com>
2024-04-25 08:16:10 +0200

    add some qol changes to main

--- main_v2.py
+++ main_v2.py
@@ -6,8 +6,10 @@ from analytics.summary import Summary
 from analytics.cache_rw import CacheRW
 from analytics.directory_manager import DirectoryMgr
 from analytics.mp4_handler import get_video_info
-from analytics.utils import (get_total_size_gb, convert_duration_to_str,
-                             convert_size_to_str, convert_resolution_to_str,
+from analytics.utils import (get_total_size_gb,
+                             convert_duration_to_str,
+                             convert_size_to_str,
+                             convert_resolution_to_str,
                              convert_size_mb_to_str)
 
 def parse_arguments() -> argparse.Namespace:
@@ -21,7 +23,7 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('--proc-speed', type=float, default=0.1,
                         help='Processing speed (GB/s)',
                         dest='proc_speed')
-    parser.add_argument('--exec-time', type=float, default=60,
+    parser.add_argument('--exec-time', type=float, default=90,
                         help='Allowed time to execute (s)',
                         dest='max_exec_time')
     return parser.parse_args()
@@ -49,19 +51,21 @@ def display_initial_info(exp_proc_time: float, list_count: int, tot_size: float,
                          proc_speed: float, rem_list_count: int,
                          total_list_count: int, total_list_size: float,
                          rem_list_size: float, proc_list_size: float) -> None:
-    print("Execution initiated:\n"
+    print("-- Execution initiated:\n"
           f"\tNumber of total files: {total_list_count}\n"
           f"\tNumber of processed files: {total_list_count - rem_list_count}\n"
           f"\tNumber of remaining files: {rem_list_count}\n"
           f"\tNumber of batch files: {list_count}\n"
           f"\tExpected time: {convert_duration_to_str(exp_proc_time)}\n"
           f"\tTotal size of batch: {convert_size_mb_to_str(tot_size * 1024)}\n"
-          f"\tAssumed processing speed: {proc_speed} GB/s")
-    print("Overall progress:\n"
+          f"\tAssumed processing speed: {proc_speed} GB/s\n"
           f"\tTotal list size: {total_list_size:.2f} GB\n"
-          f"\tRemaining list size: {rem_list_size:.2f} GB\n"
+          f"\tRemaining list size (incl batch): {rem_list_size:.2f} GB\n"
+          f"\tExpected remaining list: {rem_list_size - tot_size:.2f} GB\n"
           f"\tProcessed list size: {proc_list_size:.2f} GB\n"
-          f"\tProcessed: {proc_list_size*100/total_list_size:.2f}%")
+          f"\tAlready processed: {proc_list_size*100/total_list_size:.2f}%\n"
+          f"\tExpected progress: {(proc_list_size + tot_size)*100/total_list_size:.2f}%\n"
+          f"---------------------------------------------------------------------------")
 
 def main(args: argparse.Namespace) -> int:
     dest_dir, proj_name, separator, max_size_batch, proc_speed = process_args(args)
